4d255a35d980d5accfc78207959379bcb9728eee
Lars <ebbe1991@gmail.com>
2024-10-10 21:20:17 +0200

    first impl

--- src/sitemap_handler.py
+++ src/sitemap_handler.py
@@ -1,26 +1,43 @@
 import json
 import urllib
+import os
+import urllib.request
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 
-def lambda_handler(event, context):
-    domain = 'https://a-arbeitsrecht.de' 
+
+DOMAIN = os.getenv("DOMAIN")
+TENANT = os.getenv("TENANT")
+DYNAMIC_BLOG_ROUTES_URL = os.getenv("DYNAMIC_BLOG_ROUTES_URL")
+PAGES = os.getenv("PAGE_LIST")
+app = APIGatewayHttpResolver()
+
+
+def handle(event: dict, context: dict):
+    return app.resolve(event, context)
+
+
+@app.get('/api/sitemap')
+def get():
     routes = [
         {'path': '/', 'priority': 1.0, 'changefreq': 'weekly'},
-        {'path': '/aktuelles', 'priority': 0.9, 'changefreq': 'weekly'},
     ]
+    
+    for page in json.loads(PAGES): 
+        routes.append({'path': f'/{page}', 'priority': 0.9, 'changefreq': 'weekly'})
 
-    try:
-        dynamic_routes = get_dynamic_routes()
-        routes.extend(dynamic_routes)
-    except Exception as e:
-        print(f"Error fetching dynamic routes: {e}")
+    if DYNAMIC_BLOG_ROUTES_URL is not None:
+        try:
+            dynamic_routes = get_dynamic_blog_routes(TENANT, DYNAMIC_BLOG_ROUTES_URL)
+            routes.extend(dynamic_routes)
+        except Exception as e:
+            print(f"Error fetching dynamic routes: {e}")
 
-    # Sitemap XML erstellen
     xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
     xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
 
     for route in routes:
         xml += f'  <url>\n'
-        xml += f'    <loc>{domain}{route["path"]}</loc>\n'
+        xml += f'    <loc>{DOMAIN}{route["path"]}</loc>\n'
         xml += f'    <changefreq>{route["changefreq"]}</changefreq>\n'
         xml += f'    <priority>{route["priority"]}</priority>\n'
         xml += f'  </url>\n'
@@ -35,25 +52,21 @@ def lambda_handler(event, context):
         'body': xml
     }
 
-def get_dynamic_routes(tenant: str):
-    url = 'https://a-arbeitsrecht.de/api/blog-ids'
+def get_dynamic_blog_routes(tenant, url):
     headers = {
         'x-tenant-id': tenant
     }
 
-    # Erstelle die Anfrage
     req = urllib.request.Request(url, headers=headers)
 
     try:
-        # Sende die Anfrage und erhalte die Antwort
         with urllib.request.urlopen(req) as response:
             response_body = response.read()
             data = json.loads(response_body)
             
             dynamic_routes = []
-            # Angenommen, die API gibt eine Liste von Blog-Posts zurück
             for item in data:
-                path = f"/blog/{item['slug']}"  # Beispiel: /blog/slug
+                path = f"/blog/{item}"
                 dynamic_routes.append({
                 'path': path,
                 'priority': 0.9,
@@ -61,10 +74,8 @@ def get_dynamic_routes(tenant: str):
             })
             return dynamic_routes
     except urllib.error.HTTPError as e:
+        print(f"HTTPError: {e.code} - {e.reason}")
         return []
     except Exception as e:
+        print(f"General Error: {str(e)}")
         return []
-    url = 'https://a-arbeitsrecht.de/api/blog-ids'
-    headers = {
-        'tenant-id': 'a-arbeitsrecht.de'
-    }
