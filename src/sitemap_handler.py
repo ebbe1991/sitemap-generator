c35444f423ebd35fc1bdd7708cde58abee1197c1
Lars <ebbe1991@gmail.com>
2024-10-10 21:47:32 +0200

    fix

--- src/sitemap_handler.py
+++ src/sitemap_handler.py
@@ -3,12 +3,13 @@ import urllib
 import os
 import urllib.request
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.event_handler import Response
 
 
 DOMAIN = os.getenv("DOMAIN")
 TENANT = os.getenv("TENANT")
 DYNAMIC_BLOG_ROUTES_URL = os.getenv("DYNAMIC_BLOG_ROUTES_URL")
-PAGES = os.getenv("PAGE_LIST")
+PAGES = os.getenv("PAGES")
 app = APIGatewayHttpResolver()
 
 
@@ -16,14 +17,14 @@ def handle(event: dict, context: dict):
     return app.resolve(event, context)
 
 
-@app.get('/api/sitemap')
+@app.get("/api/sitemap")
 def get():
     routes = [
-        {'path': '/', 'priority': 1.0, 'changefreq': 'weekly'},
+        {"path": "/", "priority": 1.0, "changefreq": "weekly"},
     ]
-    
-    for page in json.loads(PAGES): 
-        routes.append({'path': f'/{page}', 'priority': 0.9, 'changefreq': 'weekly'})
+
+    for page in json.loads(PAGES):
+        routes.append({"path": f"/{page}", "priority": 0.9, "changefreq": "weekly"})
 
     if DYNAMIC_BLOG_ROUTES_URL is not None:
         try:
@@ -36,26 +37,23 @@ def get():
     xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
 
     for route in routes:
-        xml += f'  <url>\n'
+        xml += f"  <url>\n"
         xml += f'    <loc>{DOMAIN}{route["path"]}</loc>\n'
         xml += f'    <changefreq>{route["changefreq"]}</changefreq>\n'
         xml += f'    <priority>{route["priority"]}</priority>\n'
-        xml += f'  </url>\n'
+        xml += f"  </url>\n"
 
-    xml += '</urlset>'
+    xml += "</urlset>"
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/xml'
-        },
-        'body': xml
-    }
+    return Response(
+        status_code=200,
+        content_type='application/xml',
+        body=xml
+    )
+
 
 def get_dynamic_blog_routes(tenant, url):
-    headers = {
-        'x-tenant-id': tenant
-    }
+    headers = {"x-tenant-id": tenant}
 
     req = urllib.request.Request(url, headers=headers)
 
@@ -63,15 +61,13 @@ def get_dynamic_blog_routes(tenant, url):
         with urllib.request.urlopen(req) as response:
             response_body = response.read()
             data = json.loads(response_body)
-            
+
             dynamic_routes = []
             for item in data:
-                path = f"/blog/{item}"
-                dynamic_routes.append({
-                'path': path,
-                'priority': 0.9,
-                'changefreq': 'weekly'
-            })
+                path = f"/aktuelles/{item}"
+                dynamic_routes.append(
+                    {"path": path, "priority": 0.9, "changefreq": "weekly"}
+                )
             return dynamic_routes
     except urllib.error.HTTPError as e:
         print(f"HTTPError: {e.code} - {e.reason}")
